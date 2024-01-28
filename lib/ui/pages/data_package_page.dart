18e731d2ecf8c9bf8c27a481e0215a904a3894ea
Ikhwan1358 <ikhwanushofa54@gmail.com>
2024-01-28 23:47:04 +0700

    Dataplan dinamis

--- lib/ui/pages/data_package_page.dart
+++ lib/ui/pages/data_package_page.dart
@@ -1,11 +1,26 @@
+import 'package:bank_sha/models/data_plan_model.dart';
+import 'package:bank_sha/models/operator_card_model.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/button.dart';
 import 'package:bank_sha/ui/widgets/forms.dart';
 import 'package:bank_sha/ui/widgets/package_item.dart';
 import 'package:flutter/material.dart';
 
-class DataPackagePage extends StatelessWidget {
-  const DataPackagePage({super.key});
+class DataPackagePage extends StatefulWidget {
+  final OperatorCardModel operatorCard;
+
+  const DataPackagePage({
+    Key? key,
+    required this.operatorCard,
+  }) : super(key: key);
+
+  @override
+  State<DataPackagePage> createState() => _DataPackagePageState();
+}
+
+class _DataPackagePageState extends State<DataPackagePage> {
+  final phoneController = TextEditingController(text: ' ');
+  DataPlanModel? selectedDataPlan;
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +48,10 @@ class DataPackagePage extends StatelessWidget {
           const SizedBox(
             height: 14,
           ),
-          const CustomFormField(
+          CustomFormField(
             title: '+628',
             isShowTitle: false,
+            controller: phoneController,
           ),
           const SizedBox(
             height: 40,
@@ -50,46 +66,40 @@ class DataPackagePage extends StatelessWidget {
           const SizedBox(
             height: 14,
           ),
-          const Wrap(
+          Wrap(
             spacing: 17,
             runSpacing: 17,
-            children: [
-              PackageItem(
-                amount: 10,
-                price: 100000,
-              ),
-              PackageItem(
-                amount: 25,
-                price: 200000,
-                isSelected: true,
-              ),
-              PackageItem(
-                amount: 40,
-                price: 300000,
-              ),
-              PackageItem(
-                amount: 99,
-                price: 1000000,
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 135,
-          ),
-          CustomFilldButton(
-            title: 'Continue',
-            onPressed: () async {
-              if (await Navigator.pushNamed(context, '/pin') == true) {
-                Navigator.pushNamedAndRemoveUntil(
-                    context, '/data-success', (route) => false);
-              }
-            },
+            children: widget.operatorCard.dataPlans?.map((dataPlan) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedDataPlan = dataPlan;
+                      });
+                    },
+                    child: PackageItem(
+                      dataPlan: dataPlan,
+                      isSelected: dataPlan.id == selectedDataPlan?.id,
+                    ),
+                  );
+                }).toList() ??
+                [],
           ),
           const SizedBox(
             height: 57,
           ),
         ],
       ),
+      floatingActionButton:
+          (selectedDataPlan != null && phoneController.text.isNotEmpty)
+              ? Container(
+                  margin: const EdgeInsets.all(24),
+                  child: CustomFilldButton(
+                    title: 'Continue',
+                    onPressed: () {},
+                  ),
+                )
+              : Container(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
--- lib/ui/widgets/package_item.dart
+++ lib/ui/widgets/package_item.dart
@@ -1,15 +1,14 @@
+import 'package:bank_sha/models/data_plan_model.dart';
 import 'package:bank_sha/shared/shared_metods.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 class PackageItem extends StatelessWidget {
-  final int amount;
-  final int price;
+  final DataPlanModel dataPlan;
   final bool isSelected;
   const PackageItem({
     Key? key,
-    required this.amount,
-    required this.price,
+    required this.dataPlan,
     this.isSelected = false,
   }) : super(key: key);
 
@@ -37,7 +36,7 @@ class PackageItem extends StatelessWidget {
             height: 13,
           ),
           Text(
-            '${amount}GB',
+            dataPlan.name.toString(),
             style: blackTextStyle.copyWith(
               fontSize: 32,
               fontWeight: medium,
@@ -47,7 +46,7 @@ class PackageItem extends StatelessWidget {
             height: 2,
           ),
           Text(
-            formatCurrency(price),
+            formatCurrency(dataPlan.price ?? 0),
             style: greyTextStyle.copyWith(
               fontSize: 12,
             ),
